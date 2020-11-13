9c3dba9a97cb1abdc10048080486e2c98abedfd4
kentaro10 <paddle101733@gmail.com>
2020-11-13 21:31:40 +0900

    商品出品機能の実装、テストコードまで追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden_of_shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+  validates :name,           length: { maximum:40 }
+  validates :explanation,    length: { maximum:1000 }
+  validates :selling_price,  numericality: {  greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,  message: "is out of setting range"}
+  validates :image    
+  end
+  validates :category_id,  numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :burden_of_shipping_charge_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :days_to_ship_id, numericality: { other_than: 0 }
+
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name          {"犬"}
+    explanation   {"生まれたてです"}
+    selling_price {"10001"}
+    category_id   {"2"}
+    status_id     {"2"}
+    burden_of_shipping_charge_id  {"2"}
+    shipping_area_id              {"2"}
+    days_to_ship_id               {"2"}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
