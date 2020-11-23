4d18668a027625d31152de5a00d9a006acf35f26
kentaro10 <paddle101733@gmail.com>
2020-11-23 16:31:32 +0900

    購入済みの商品は編集ページに飛べないように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy ]
   before_action :move_to_index, only: :edit
-  
+  before_action :move_to_index_2, only: :edit
   def index
     @items = Item.includes(:user).order("created_at DESC")
     
@@ -62,5 +62,11 @@ class ItemsController < ApplicationController
    unless user_signed_in?  && current_user.id == @item.user_id 
     redirect_to action: :index
    end
+  end
+
+  def move_to_index_2
+    unless  @item.order.blank?
+      redirect_to action: :index
+    end
   end
 end
